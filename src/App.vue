313a4225526fe151ee3fd1dabc43ce38b7441d44
RikZun <rik.zunqq@gmail.com>
2021-03-27 00:14:34 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,55 @@
   <div class="container">
     <div class="block">
       <div>
-        <h2>Общая информация</h2>
+        <h3>Общая информация</h3>
         <hr>
       </div>
-      <Form />
+      <div class="table">
+        <Form :label="'Фамилия*'" :placeholder="'Пушкин'" />
+        <Form :label="'Имя*'" :placeholder="'Александр'" />
+        <Form :label="'Отчество'" :placeholder="'Сергеевич'" />
+        <Form :label="'Дата рождения*'" :placeholder="'26/05/1799'" />
+        <Form :label="'Номер телефона*'" :placeholder="'78009000000'" />
+        <Form :label="'Пол'" :placeholder="'Мужской'" />
+        <Form :label="'Группа клиентов*'" :options="['', 'VIP', 'Проблемные', 'ОМС']" />
+        <Form :label="'Лечащий врач'" :options="['', 'Иванов', 'Захаров', 'Чернышева']" />
+      </div>
+      
+      <div>
+        <h3>Адрес</h3>
+        <hr>
+      </div>
+      <div class="table">
+        <Form :label="'Индекс'" :placeholder="'101000'" />
+        <Form :label="'Страна'" :placeholder="'Россия'" />
+        <Form :label="'Область'" :placeholder="'Московская область'" />
+        <Form :label="'Город*'" :placeholder="'Москва'" />
+        <Form :label="'Улица'" :placeholder="'Свободы'" />
+        <Form :label="'Дом'" :placeholder="'21'" />
+      </div>
+
+      <div>
+        <h3>Паспорт</h3>
+        <hr>
+      </div>
+      <div class="table">
+        <Form :label="'Тип документа*'" :options="['', 'Паспорт', 'Свидетельство о рождении', 'Вод. удостоверение']" />
+        <Form :label="'Серия'" :placeholder="'45 15'" />
+        <Form :label="'Номер'" :placeholder="'020105'" />
+        <Form :label="'Кем выдан'" :placeholder="'ОТДЕЛОМ УФМС РОССИИ ПО ГОР. МОСКВЕ'" :length="2" />
+        <Form :label="'Дата выдачи*'" :placeholder="'26/05/1813'" />
+      </div>
+
+      <div class="end">
+        <label><input type="checkbox" checked>Отправить СМС</label>
+        <button type="button">Создать учётную запись</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import 'vuelidate'
 import Vue from 'vue'
 import Form from '@/components/form.vue'
 
@@ -23,13 +63,6 @@ export default Vue.extend({
 </script>
 
 <style lang="sass">
-body
-  font-family: SF Pro Display, Regular
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  height: 100vh
-  margin: 0
-
 @font-face
   font-family: 'SF Pro Display'
   src: local('SF Pro Display Regular'), local('SFProDisplay-Regular'), url('assets/fonts/SFProDisplay-Regular.woff2') format('woff2'), url('assets/fonts/SFProDisplay-Regular.woff') format('woff'), url('assets/fonts/SFProDisplay-Regular.ttf') format('truetype')
@@ -48,15 +81,76 @@ body
   font-weight: 200
   font-style: normal
 
+@media screen and (max-width: 990px)
+  .table
+    grid-template-columns: repeat(2, 1fr) !important
+
+  .table>.group[style]
+    grid-column-end: span 1 !important
+
+@media screen and (max-width: 510px)
+  .table
+    grid-template-columns: repeat(1, 1fr) !important
+    grid-row-gap: unset !important
+
+  .table>.group[style]
+    grid-column-end: span 1 !important
+
+body
+  font-family: SF Pro Display, Regular !important
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  margin: 0
+
 .container
   display: flex
   justify-content: center
-  width: inherit
-  height: inherit
   align-items: center
 
 .block
   justify-content: center
   display: flex
   flex-direction: column
+
+h3
+  margin-top: 50px
+
+.table
+  display: grid
+  grid-template-columns: repeat(3, 1fr)
+  grid-template-rows: auto
+  grid-gap: 10px
+
+.end
+  margin-block: 20px
+  display: flex
+  flex-direction: column
+  align-items: center
+  height: 70px
+  justify-content: space-around
+
+  >label
+    font-size: 12px
+
+    >input
+      vertical-align: sub
+
+button
+  display: flex;
+  padding: 10px 20px
+  background-color: black
+  color: white
+  border: none
+  border-radius: 36px
+  cursor: pointer
+
+  &:hover
+    opacity: .8
+    transition: opacity 200ms linear
+
+  &:focus
+    outline: none
+
+.error
+  border: 1px solid #ff0000
 </style>
--- src/components/form.vue
+++ src/components/form.vue
@@ -1,46 +1,38 @@
 <template>
-  <div class="table">
-    <div class="group">
-      <label class="control-label" for="nome">Имя*</label>
-      <input name="nome" class="form" placeholder="Константин" type="text">
-    </div>
-    <div class="group">
-      <label class="control-label" for="nome">Фамилия*</label>
-      <input name="nome" class="form" placeholder="Циолковский" type="text">
-    </div>
-    <div class="group">
-      <label class="control-label" for="nome">Отчество</label>
-      <input name="nome" class="form" placeholder="Эдуардович" type="text">
-    </div>
-    <div class="group">
-      <label class="control-label" for="nome">Дата рождения*</label>
-      <input name="nome" class="form" placeholder="17/09/1857" type="text">
-    </div>
-    <div class="group">
-      <label class="control-label" for="nome">Номер телефона*</label>
-      <input name="nome" class="form" placeholder="78009003541" type="text">
-    </div>
-    <div class="group">
-      <label class="control-label" for="nome">Пол</label>
-      <input name="nome" class="form" placeholder="Мужской" type="text">
-    </div>
-    <div class="group" style="grid-area: 3 / 1 / 4 / 4">
-      <label class="control-label" for="nome">Поле</label>
-      <input name="nome" class="form" placeholder="Поле" type="text">
-    </div>
+  <div class="group" :style="{gridColumnEnd: length ? 'span ' + length : ''}">
+    <label>{{ label }}</label>
+
+    <select v-if="options">
+      <option v-for="option of options" :key="option">
+        {{ option }}
+      </option>
+    </select>
+
+    <input v-else :placeholder="placeholder" type="text">
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    label: {
+      required: true
+    },
+    placeholder: {},
+    length: {
+      type: Number
+    },
+    options: {
+      type: Array
+    }
+  },
+  data() { return { val: '' } },
+})
+</script>
+
 <style scoped lang="sass">
-@media screen and (max-width: 990px)
-  .table
-    grid-template-columns: repeat(2, 1fr) !important
-
-@media screen and (max-width: 510px)
-  .table
-    grid-template-columns: repeat(1, 1fr) !important
-    grid-row-gap: unset !important
-
 .group
   display: flex
   flex-direction: column
@@ -51,19 +43,23 @@ label
   margin: 14px 0 3px 0
   display: inline-block
 
-.form
-  display: block
-  height: 14px
-  padding: 6px 12px
+%input-block
   font-size: 14px
-  line-height: 1.42857143
-  color: #555
   border: 1px solid #D3D3D3
   border-radius: 6px
 
-.table
-  display: grid
-  grid-template-columns: repeat(3, 1fr)
-  grid-template-rows: repeat(5, 1fr)
-  grid-gap: 10px
+input
+  display: block
+  height: 14px
+  padding: 6px 12px
+  @extend %input-block
+
+  &::placeholder
+     color: #cecece
+
+select
+  @extend %input-block
+  height: 28px
+  padding-inline: 8px
+
 </style>
